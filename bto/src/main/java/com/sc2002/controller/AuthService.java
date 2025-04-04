c72dd445999f8093013be52a1f95c7865f450aaf
Tew Huan Sheng <sheng1461@gmail.com>
2025-04-04 11:02:19 +0800

    Clean up the code a bit

--- bto/src/main/java/com/sc2002/controller/AuthService.java
+++ bto/src/main/java/com/sc2002/controller/AuthService.java
@@ -1,18 +1,19 @@
 package com.sc2002.controller;
 
+import com.sc2002.enums.UserRole;
 import com.sc2002.model.User;
-import com.sc2002.enums.*;
+
 public class AuthService {
-    public boolean isOfficer(User currentUser){
-        if(currentUser.getUsersRole()==UserRole.HDB_OFFICER)return true;
-        return false;
+
+    public boolean isOfficer(User currentUser) {
+        return currentUser.getUsersRole() == UserRole.HDB_OFFICER;
     }
-    public boolean isManager(User currentUser){
-        if(currentUser.getUsersRole()==UserRole.HDB_MANAGER)return true;
-        return false;
+
+    public boolean isManager(User currentUser) {
+        return currentUser.getUsersRole() == UserRole.HDB_MANAGER;
     }
-    public boolean isApplicant(User currentUser){
-        if(currentUser.getUsersRole()==UserRole.APPLICANT)return true;
-        return false;
+
+    public boolean isApplicant(User currentUser) {
+        return currentUser.getUsersRole() == UserRole.APPLICANT;
     }
 }
